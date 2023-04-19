7ad3c5a3976e219928aa58df35ecbc8ca495c091
Ildar <ildar.sadykov2000@gmail.com>
2023-04-19 19:09:34 +0300

    add more test

--- test/test.cpp
+++ test/test.cpp
@@ -7,6 +7,13 @@ TEST(Temperature, CelsiusToKelvin)
     ASSERT_DOUBLE_EQ(CelsiusToKelvin(-10), 263.15);
     ASSERT_DOUBLE_EQ(CelsiusToKelvin(0), 273.15);
     ASSERT_DOUBLE_EQ(CelsiusToKelvin(10), 283.15);
+    ASSERT_DOUBLE_EQ(CelsiusToKelvin(102), 375.15);
+    ASSERT_DOUBLE_EQ(CelsiusToKelvin(200), 473.15);
+    ASSERT_DOUBLE_EQ(CelsiusToKelvin(-11), 262.15);
+    ASSERT_DOUBLE_EQ(CelsiusToKelvin(-126.928), 146.222);
+    ASSERT_DOUBLE_EQ(CelsiusToKelvin(-196.234), 76.916);
+    ASSERT_DOUBLE_EQ(CelsiusToKelvin(-204.800), 68.35);
+    ASSERT_DOUBLE_EQ(CelsiusToKelvin(94.872), 368.022);
 }
 
 TEST(Temperature, KelvinToCelsius)
@@ -14,6 +21,10 @@ TEST(Temperature, KelvinToCelsius)
     ASSERT_DOUBLE_EQ(KelvinToCelsius(-10), -283.15);
     ASSERT_DOUBLE_EQ(KelvinToCelsius(0), -273.15);
     ASSERT_DOUBLE_EQ(KelvinToCelsius(10), -263.15);
+    ASSERT_DOUBLE_EQ(CelsiusToKelvin(102), 375.15);
+    ASSERT_DOUBLE_EQ(CelsiusToKelvin(200), 473.15);
+    ASSERT_DOUBLE_EQ(CelsiusToKelvin(-11), 262.15);
+    ASSERT_DOUBLE_EQ(KelvinToCelsius(94.872), -178.278);
 }
 
 TEST(Temperature, Illumination)
